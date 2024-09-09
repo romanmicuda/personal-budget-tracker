4fff5b365d6c6d864ce4d275fecafff2c55e8b67
Roman Micuda <roman.micuda@outlook.com>
2024-09-10 00:09:58 +0200

    feat: save data to localstorage

--- src/pages/PersonalBudgetTracker.tsx
+++ src/pages/PersonalBudgetTracker.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Category from "../components/Category";
 import { CategoryType, TransactionType } from "../types";
 import { initialCategories } from "../data";
@@ -6,21 +6,59 @@ import { Transaction } from "../components/Transaction";
 import { DisplayTransactions } from "../components/DisplayTransactions";
 import { Balance } from "./Balance";
 import Chart from "../components/Chart";
+
+const parseTransactions = (transactions: TransactionType[]): any[] => {
+  return transactions.map((transaction: TransactionType) => ({
+    ...transaction,
+    date: new Date(transaction.date),
+  }));
+};
+
+const stringifyTransactions = (transactions: TransactionType[]): any[] => {
+  return transactions.map((transaction) => ({
+    ...transaction,
+    date: transaction.date.toISOString(),
+  }));
+};
+
 function PersonalBudgetTracker() {
-  const [categories, setCategories] =
-    useState<CategoryType[]>(initialCategories);
-  const [transactions, setTransactions] = useState<TransactionType[]>([]);
+  const [categories, setCategories] = useState<CategoryType[]>(() => {
+    const storedCategories = localStorage.getItem("categories");
+    return storedCategories ? JSON.parse(storedCategories) : initialCategories;
+  });
+
+  const [transactions, setTransactions] = useState<TransactionType[]>(() => {
+    const storedTransactions = localStorage.getItem("transactions");
+    return storedTransactions
+      ? parseTransactions(JSON.parse(storedTransactions))
+      : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("categories", JSON.stringify(categories));
+  }, [categories]);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "transactions",
+      JSON.stringify(stringifyTransactions(transactions))
+    );
+  }, [transactions]);
+
   const addCategory = (category: string) => {
     setCategories((previous) => [...previous, { name: category }]);
   };
+
   const addTransaction = (transaction: TransactionType) => {
     setTransactions((previous) => [...previous, transaction]);
   };
 
   const deleteTransaction = (transaction: TransactionType) => {
-    setTransactions((previous) => [
-      ...previous.filter((trans) => trans.date != transaction.date),
-    ]);
+    setTransactions((previous) =>
+      previous.filter(
+        (trans) => trans.date.getTime() !== transaction.date.getTime()
+      )
+    );
   };
 
   return (
@@ -32,7 +70,7 @@ function PersonalBudgetTracker() {
         addTransaction={addTransaction}
       />
       <Category categories={categories} addCategory={addCategory} />
-      {transactions.length !== 0 && <Chart transactions={transactions} />}
+      {transactions.length !== 0 ? <Chart transactions={transactions} /> : null}
       <DisplayTransactions
         transactions={transactions}
         deleteTransaction={deleteTransaction}
